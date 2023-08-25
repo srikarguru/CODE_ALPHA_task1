cabd8b945ad6af3f84c467cf3265a5b0b2180a3e
srikarguru <143164115+srikarguru@users.noreply.github.com>
2023-08-25 12:17:07 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+package Attendance;
+import java.sql.*;
+
+public class Main {
+
+	public static void main(String[] args) throws SQLException {
+		Login login = new Login();
+		login.loginView();
+	}
+}
